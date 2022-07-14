b9b2bcad4b49f2a83660717c2894144a3ef825af
Johnny <johnny.san@gmail.com>
2022-07-14 13:51:45 -0700

    added notes to like component in typescript

--- ts-hello/likesComponent.ts
+++ ts-hello/likesComponent.ts
@@ -1,18 +1,18 @@
-export class likesComponent {
-    constructor(private _likesCount:number, private _isSelected:boolean) {
+export class likesComponent { //likes component
+    constructor(private _likesCount:number, private _isSelected:boolean) {//the variables are private so they cannot be reassigned
     }
 
     onClick() {
-        this._likesCount += (this._isSelected) ? -1: +1;
-        this._isSelected = !this._isSelected;
+        this._likesCount += (this._isSelected) ? -1: +1; //if the like button is visible, decrease total likes, else increase total likes
+        this._isSelected = !this._isSelected; //once like button is clicked, set selected to !selected
     }
 
     get likesCount() {
-        return this._likesCount;
+        return this._likesCount;//getter for like count
     }
 
     get isSelected() {
-        return this._isSelected;
+        return this._isSelected; //getter for selected state
     }
 }
 
